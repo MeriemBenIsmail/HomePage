c7a1697414801e46ec9b3b648a99c75d2ecb5cf6
meriem ben ismail <meriembenismail@yahoo.com>
2021-04-08 21:44:03 +0100

    heart icon fixed

--- js/heart.js
+++ js/heart.js
@@ -1,11 +1,7 @@
-var heart=document.querySelectorAll('#heart');
+var like=document.querySelectorAll('.like');
 
-heart.forEach((element) => {
-  element.addEventListener('click',(e)=>{
-    console.log(e);
- 
-    element.parentNode.removeChild(element);
-   
-    
+like.forEach((element) => {
+  element.addEventListener('click',()=>{
+    element.classList.toggle('heart');
   })
 })
