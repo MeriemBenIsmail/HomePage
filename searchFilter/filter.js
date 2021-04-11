304bacd207e6e1f14d6e56d65588ca0cae3f1a91
meriem ben ismail <meriembenismail@yahoo.com>
2021-04-11 12:33:10 +0100

    links

--- searchFilter/filter.js
+++ searchFilter/filter.js
@@ -14,19 +14,21 @@ for(let i=0;i<indicator.length;i++){
     
     for(let k=0;k<main.length;k++){
 
-      main[k].style.transform='scale(0)';
+      //main[k].style.transform='scale(0)';
       main[k].style.display='none';
-      /*setTimeout(()=>{
+     /* setTimeout(()=>{
         main[k].style.display='none';
-      },500);*/
+        
+      },200);*/
 
 
       if((main[k].getAttribute('data-category')== displayItems) || (displayItems== 'all')){
         console.log(main[k]);
-        main[k].style.transform='scale(1)';
+        //main[k].style.transform='scale(1)';
         main[k].style.display='block';
        /* setTimeout(()=>{
           main[k].style.display='block';
+          
         },200);*/
         
       }
